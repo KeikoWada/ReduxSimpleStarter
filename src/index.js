83e02a9174a21bd2fc2e5666219ace306153753a
KeikoWada <kwaddy71385@gmail.com>
2018-06-01 16:28:50 -0400

    Install npm youtube-api-search and created the component called SearchBar under App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+// google youtube API_KEY
+const API_KEY = 'AIzaSyDb0nunIa_PkItmSDsSo2cnyqY6VSGnmK8'
+
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML, and put it
